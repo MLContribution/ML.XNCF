bd2d524262bb7461f3abac5430679f53975b3bf6
zhao365845726@163.com <zhao365845726@163.com>
2021-02-17 07:07:10 +0800

    增加swagger模块

---
+++ ML.Xncf.Swagger/Register.Middleware.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Options;
+using Senparc.Ncf.XncfBase;
+using ML.Xncf.Swagger.Builder;
+using System;
+using System.IO;
+
+namespace ML.Xncf.Swagger
+{
+    public partial class Register : IXncfMiddleware
+    {
+        public IApplicationBuilder UseMiddleware(IApplicationBuilder app)
+        {
+            app.UseSwaggerCustom();
+            return app;
+        }
+    }
+}
